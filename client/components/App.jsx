56fd2c729790f2730b126cc02b1f556918fca575
Mattias Sämskar <mattias.samskar@gmail.com>
2017-07-04 16:15:07 +0200

    Add Item and ItemList components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  state = {
+    items: [
+      {
+        name: 'milk'
+      },
+        {
+        name: 'butter'
+      },
+        {
+        name: 'soap'
+      }
+    ]
+  }
+
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
-  }
+      <div>
+        <ItemList items={this.state.items}/>
+      </div>
+    );
+  };
+};
+
+const ItemList = (props) => {
+  return (
+    <div>
+      {props.items.map(item => <Item {...item} />)}
+    </div>
+  );
+}
+
+const Item = (props) => {
+  return <div>{props.name}</div>;
 }
