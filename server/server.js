9b95970daa5ca4adf9187c98ad02861642887c78
Mattias Sämskar <mattias.samskar@gmail.com>
2017-07-07 12:15:42 +0200

    Add sortOrder to Item and reorder endpoint

--- server/server.js
+++ server/server.js
@@ -9,44 +9,45 @@ var items = [
   {
     id: '1',
     name: 'milk',
-    isActive: true
+    isActive: true,
+    sortOrder: 3
   },
   {
     id: '2',
     name: 'butter',
-    isActive: true
+    isActive: true,
+    sortOrder: 2
   },
   {
     id: '3',
     name: 'candy',
-    isActive: false
+    isActive: false,
+    sortOrder: 1
   }
 ];
 
 io.on('connection', (socket) => {
-  io.to(socket.id).emit('items', items);
+  io.to(socket.id).emit('items', getItems());
 });
 
 app.get('/items', (req, res) => {
-  res.send(items);
+  res.send(getItems());
 });
 
 app.post('/items', (req, res) => {
-  const newItem = {
+  const item = {
     id: Math.random().toString(),
     name: req.body.name,
     isActive: req.body.isActive
   };
-  items = items.concat(newItem);
+  addItem(item);
 
   res.status(200).send();
-  io.emit('items', items);
+  io.emit('items',  getItems());
 });
 
 app.put('/items/:id', (req, res) => {
-  var item = items.find((i) => {
-    return i.id === req.params.id;
-  });
+  var item = getItemById(req.params.id);
 
   if (!item) {
     res.status(404).send();
@@ -55,8 +56,41 @@ app.put('/items/:id', (req, res) => {
   item.isActive = req.body.isActive;
 
   res.status(200).send();
-  io.emit('items', items);  
+  io.emit('items', getItems());
 });
+
+app.post('/items/reorder', (req, res) => {
+  var item1 = getItemById(req.params.itemIds[0]);
+  var item2 = getItemById(req.params.itemIds[1]);
+  
+  const sortOrder1 = item1.sortOrder;
+  const sortOrder2 = item2.sortOrder;
+
+  item1.sortOrder = sortOrder2;
+  item2.sortOrder = sortOrder1;
+
+  res.status(200).send();
+  io.emit('items', getItems());
+});
+
+const getItemById = (id) => {
+  return getItems().find((item) => {
+    return item.id === id;
+  });
+};
+
+const getItems = () => {
+  items.sort((item1, item2) => {
+    return item1.sortOrder - item2.sortOrder;
+  });
+  return items;
+};
+
+const addItem = (item) => {
+  const sortOrders = getItems().map((item) => item.sortOrder);
+  item.sortOrder = Math.max(...sortOrders) + 1;
+  items = items.concat(item);
+};
 
 server.listen(3000, () => {
   console.log('Listening on port 3000')
